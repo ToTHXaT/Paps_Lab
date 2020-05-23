e91907ab2190d0f2cbb0c60e85d2c3ccff6d14b9
Ainur <ainur-205@yandex.ru>
2020-05-23 14:21:15 +0300

    Played with admin

--- skud/db/admin.py
+++ skud/db/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Department, Worker, Client, AccessRights, Report
 
-# Register your models here.
+
+class NAdmin(admin.ModelAdmin):
+    search_fields = ('fio',)
+
+class ARAdmin(admin.ModelAdmin):
+
+    list_filter = ('department__name',)
+    search_fields = ('client__fio',)
+
+admin.site.register(Department)
+admin.site.register(Worker)
+admin.site.register(Client, NAdmin)
+admin.site.register(AccessRights, ARAdmin)
+admin.site.register(Report)
--- skud/db/models.py
+++ skud/db/models.py
@@ -5,8 +5,10 @@ class Department(models.Model):
     name = models.CharField(max_length = 200)
     sub_dep_count = models.IntegerField()
 
-    sup_dep = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+    sup_dep = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f"[{self.name}]"
 
 class Worker(models.Model):
 
@@ -16,12 +18,17 @@ class Worker(models.Model):
     login = models.CharField(max_length = 50)
     password = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return f"<{self.fio} from {self.department}>"
 
 class Client(models.Model):
 
-    pass_id = models.IntegerField()
+    pass_id = models.IntegerField(unique=True)
     fio = models.CharField(max_length = 200)
     info = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return f"{self.fio} : {self.pass_id}"
 
 
 class AccessRights(models.Model):
@@ -29,6 +36,11 @@ class AccessRights(models.Model):
     count_constrait = models.IntegerField()
 
     department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, to_field='pass_id', null=True)
+
+    def __str__(self):
+        return f"Access right for {self.client} on {self.department}"
+
 
 
 
@@ -37,3 +49,6 @@ class Report(models.Model):
     worker = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True)
 
     data = models.TextField()
+
+    def __str__(self):
+        return f"Report by {self.worker}\n{self.data}"
